c2df159a7724510caae516975477089319217706
Moussagouba <50293163+Moussagouba@users.noreply.github.com>
2020-04-08 04:48:21 +0000

    Temp.py
    Ce Programme permet de convertir l'age en jour minutes et en seconde

---
+++ moo.py
@@ -0,0 +1,8 @@
+print("Voyons voir combien de temps vous avez vécu en nombre de jours, de minutes et de secondes avec ce programme de Moussa .")
+nom=input("votre nom : ")
+print("entrez maintenant votre âge") 
+age=int(input("age :"))
+jours=age* 365 
+minutes=age* 525948 
+secondes=age * 31556926
+print(nom, "a vécu", jours,"jours", minutes, "minutes et", secondes, "secondes ! Wow !")
